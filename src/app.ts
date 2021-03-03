533bf6593525967e679158d2fa5c2bf5800da13d
sweisz <dev@steffenweisz.de>
2021-03-03 09:25:15 +0100

    Add DB Connection

--- src/app.ts
+++ src/app.ts
@@ -2,24 +2,52 @@ import { printWelcomeMessage, printNoAccess } from "./messages";
 import { askForAction, askForCredentials } from "./questions";
 import { handleGetPassword, handleSetPassword, hasAccess } from "./commands";
 
-const run = async () => {
-  printWelcomeMessage();
-  const credentials = await askForCredentials();
-  if (!hasAccess(credentials.masterPassword)) {
-    printNoAccess();
-    run();
-    return;
-  }
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+dotenv.config();
 
-  const action = await askForAction();
-  switch (action.command) {
-    case "set":
-      handleSetPassword(action.passwordName);
-      break;
-    case "get":
-      handleGetPassword(action.passwordName);
-      break;
+const run = async () => {
+  const url = process.env.MONGODB_URL;
+
+  try {
+    const client = await MongoClient.connect(url, {
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to DB!");
+
+    const db = client.db("Rescue-steffen");
+
+    await db.collection("inventory").insertOne({
+      item: "canvas",
+      qty: 100,
+      tags: ["cotton"],
+      size: { h: 28, w: 35.5, uom: "cm" },
+    });
+
+    client.close();
+  } catch (error) {
+    console.error(error);
   }
 };
+
+// const run = async () => {
+//   printWelcomeMessage();
+//   const credentials = await askForCredentials();
+//   if (!hasAccess(credentials.masterPassword)) {
+//     printNoAccess();
+//     run();
+//     return;
+//   }
+
+//   const action = await askForAction();
+//   switch (action.command) {
+//     case "set":
+//       handleSetPassword(action.passwordName);
+//       break;
+//     case "get":
+//       handleGetPassword(action.passwordName);
+//       break;
+//   }
+// };
 
 run();
