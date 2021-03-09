9c23f99dd8fcae81cad631cae73abb58886ca93a
sweisz <dev@steffenweisz.de>
2021-03-09 22:49:46 +0100

    ✨ Add styled-components

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,32 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const StyledSubmitButton = styled.button`
+  color: red;
+`;
+
+const Container = styled.div`
+  display: grid;
+  width: 100vw;
+  height: 100vh;
+  place-items: center;
+  /* justify-items: center;
+  align-items: center; */
+`;
+
+const PassName = styled.p`
+  background-color: lime;
+  color: black;
+  font-size: 3rem;
+  padding: 1em;
+`;
+
+const Pass = styled.p`
+  background-color: black;
+  color: lime;
+  font-size: 3rem;
+  padding: 1em;
+`;
 
 export default function Home() {
   const [passwordName, setPasswordName] = useState("");
@@ -14,18 +42,22 @@ export default function Home() {
   }
   return (
     <>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <input
-          value={passwordName}
-          onChange={(event) => setPasswordName(event.target.value)}
-        />
-        <button type="submit">Abfrage senden!</button>
-      </form>
-      {passwordDoc && (
-        <>
-          {passwordDoc.name} {passwordDoc.value}
-        </>
-      )}
+      <Container>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            value={passwordName}
+            onChange={(event) => setPasswordName(event.target.value)}
+          />
+          <StyledSubmitButton type="submit">Send request!</StyledSubmitButton>
+        </form>
+        {passwordDoc && (
+          <>
+            <PassName> {passwordDoc.name} </PassName>
+            <Pass>{passwordDoc.value}</Pass>
+          </>
+        )}
+        <input type="password" />
+      </Container>
     </>
   );
 }
