7444d311621867ae323fcea9d5c0a2db1e45a188
sweisz <dev@steffenweisz.de>
2021-03-10 01:05:11 +0100

    ✨ Add a color changing input

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,7 +2,15 @@ import { useState } from "react";
 import styled from "styled-components";
 
 const StyledSubmitButton = styled.button`
-  color: red;
+  margin: 1em;
+  border: none;
+  background-color: 0e6d6d;
+  padding: 1em;
+  border-radius: 1.2em;
+  &:hover {
+    background: rgb(10, 61, 61);
+    color: white;
+  }
 `;
 
 const Container = styled.div`
@@ -10,27 +18,62 @@ const Container = styled.div`
   width: 100vw;
   height: 100vh;
   place-items: center;
-  /* justify-items: center;
-  align-items: center; */
+  background: #fdc830; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #f37335,
+    #fdc830
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #f37335,
+    #fdc830
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 `;
 
 const PassName = styled.p`
   background-color: lime;
+  border-radius: 1em;
   color: black;
   font-size: 3rem;
-  padding: 1em;
+  padding: 0.5em 1em;
 `;
 
 const Pass = styled.p`
   background-color: black;
+  border-radius: 1em;
   color: lime;
   font-size: 3rem;
-  padding: 1em;
+  padding: 0.5em 1em;
+`;
+
+const InputSearch = styled.input`
+  border: none;
+  padding: 0.9em;
+  border-radius: 0.7em;
+`;
+
+type Props = {
+  value: string;
+};
+
+function calcColor(length: number): string {
+  if (length === 0) {
+    return "red";
+  } else if (length < 10) {
+    return "orange";
+  }
+  return "green";
+}
+
+const SafeInput = styled.input<Props>`
+  background: ${(props) => calcColor(props.value.length)};
 `;
 
 export default function Home() {
   const [passwordName, setPasswordName] = useState("");
   const [passwordDoc, setPasswordDoc] = useState(null);
+  const [secret, setSecret] = useState("");
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -44,7 +87,7 @@ export default function Home() {
     <>
       <Container>
         <form onSubmit={(e) => handleSubmit(e)}>
-          <input
+          <InputSearch
             value={passwordName}
             onChange={(event) => setPasswordName(event.target.value)}
           />
@@ -56,7 +99,11 @@ export default function Home() {
             <Pass>{passwordDoc.value}</Pass>
           </>
         )}
-        <input type="password" />
+        <SafeInput
+          value={secret}
+          onChange={(event) => setSecret(event.target.value)}
+          type="password"
+        />
       </Container>
     </>
   );
